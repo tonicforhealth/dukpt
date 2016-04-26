e487aab91300c7d9df062ac4331f7de2c56aff55
Vitalii Ekert <vitalii.ekert@tonicforhealth.com>
2016-04-26 10:56:57 +0300

    Update copyright

--- tests/Key/DerivedKeyTest.php
+++ tests/Key/DerivedKeyTest.php
@@ -4,7 +4,7 @@
  *
  * Copyright (c) 2016 Tonic Health <info@tonicforhealth.com>
  *
- * For the full copyright and license information, please view the LICENSE
+ * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
  */
 namespace TonicForHealth\DUKPT\Tests\Key;
