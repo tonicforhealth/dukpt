67e60beada03a82fcabd5952f5de1e5ed47be9e7
Vitalii Ekert <vitalii.ekert@tonicforhealth.com>
2017-04-12 00:21:23 +0300

    Rename the LICENSE.md file to LICENSE

--- src/DUKPTFactory.php
+++ src/DUKPTFactory.php
@@ -4,7 +4,7 @@
  *
  * Copyright (c) 2016-2017 Tonic Health <info@tonicforhealth.com>
  *
- * For the full copyright and license information, please view the LICENSE.md
+ * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 namespace TonicForHealth\DUKPT;
